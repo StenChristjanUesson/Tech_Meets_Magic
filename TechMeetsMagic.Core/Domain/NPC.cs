2085975d6b81979175f6063c2ef24b2123f50240
Opilane TTHK <StenUesson@gmail.com>
2024-10-14 15:45:56 +0300

    Twin Models Created and appsetting updated

---
+++ TechMeetsMagic.Core/Domain/NPC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMeetsMagic.Core.Domain
+{
+    public enum NpcType
+    {
+        QuestGiver, Companion, Researcher, Hostile, Tutorial, 
+    }
+    public enum NPCStatus
+    {
+        Dead, Paralized, InFear, KnockedOut, Sleeping
+    }
+    internal class NPC
+    {
+        public Guid ID { get; set; }
+        public string NPCName { get; set; }
+        public string NPCDescribtion { get; set; }
+        public NPCStatus NPCStatus { get; set; }
+        public int NPCMaxHP { get; set; }
+        public int NPCCurrentHP { get; set; }
+        public int NPCAttackDamage {  get; set; }
+        public NpcType NpcType { get; set; }
+
+        //db only
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set;}
+    }
+}
---
+++ TechMeetsMagic/Models/NonPlayerCharacters/NPCIndexViewModels.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.NetworkInformation;
+
+namespace TechMeetsMagic.Models.NonPlayerCharacters
+{
+    public enum NpcType
+    {
+        QuestGiver, Companion, Researcher, Hostile, Tutorial,
+    }
+    public enum NPCStatus
+    {
+        Dead, Paralized, InFear, KnockedOut, Sleeping
+    }
+    public class NPCIndexViewModels
+    {
+        public Guid ID { get; set; }
+        public string NPCName { get; set; }
+        public string NPCDescribtion { get; set; }
+        public NPCStatus NPCStatus { get; set; }
+        public int NPCMaxHP { get; set; }
+        public int NPCCurrentHP { get; set; }
+        public int NPCAttackDamage { get; set; }
+        public NpcType NpcType { get; set; }
+
+        //db only
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
