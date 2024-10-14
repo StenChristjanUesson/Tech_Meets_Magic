507d52866c01cd2be501661f485ee301dc7b1490
Opilane TTHK <StenUesson@gmail.com>
2024-10-14 16:24:27 +0300

    Context is still a work in progress

--- TechMeetsMagic.Data/TechMeetsMagicContext.cs
+++ TechMeetsMagic.Data/TechMeetsMagicContext.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using TechMeetsMagic.Core.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TechMeetsMagic.Data
 {
-    internal class TechMeetsMagicContext
+    internal class TechMeetsMagicContext : DbContext
     {
-
+        public DbSet<NPC> NPCs { get; set; }
     }
 }
