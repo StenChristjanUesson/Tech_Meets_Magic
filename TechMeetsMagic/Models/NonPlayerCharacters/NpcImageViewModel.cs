3cbba004d86e4b9451d3af67af6d37c61b4b780e
Opilane TTHK <opilane@tthk.ee>
2024-10-21 16:31:22 +0300

    ahhhhhh....
    ahhhhhhhhhhhhhhahhhhhhhhaaaaaaaaaaaaaaaaaaaaaaaaaaaah

---
+++ TechMeetsMagic/Models/NonPlayerCharacters/NpcImageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TechMeetsMagic.Models.NonPlayerCharacters
+{
+    public class NpcImageViewModel
+    {
+        public Guid ImageID { get; set; }
+        public string ImageTitle { get; set; }
+        public byte[] ImageData { get; set; }
+        public string image { get; set; }
+        public Guid? NpcID { get; set; }
+    }
+}
