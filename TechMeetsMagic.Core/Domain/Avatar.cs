4be98fd34e4536c20ccf0cdaeeffe4400313eebe
opilane <StenUesson@gmail.com>
2024-11-15 13:22:00 +0200

    Avatar is being made

---
+++ TechMeetsMagic.Core/Domain/Avatar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechMeetsMagic.Core.Domain
+{
+    public enum AvatarGender
+    {
+        Male, Female, Monster, Machine
+    }
+    public enum AvatarStatus
+    {
+        Dead, Paralized, Fear, KnockedOut, Sleeping
+    }
+    internal class Avatar
+    {
+        public Guid ID { get; set; }
+        public string? AvatarDescribtion { get; set; }
+        public int AvatarLevel { get; set; }
+        public int AvatarMaxHP { get; set; }
+        public int AvatarCurrentHP { get; set; }
+        public AvatarStatus AvatarStatus { get; set; }
+        public int AvatarAttackDamage { get; set; }
+        public AvatarGender AvatarGender { get; set; }
+
+        //db only
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ TechMeetsMagic/Controllers/AvatarsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechMeetsMagic.ApplicationsServices.Services;
+using TechMeetsMagic.Core.ServicesInterface;
+using TechMeetsMagic.Data;
+using TechMeetsMagic.Models.NonPlayerCharacters;
+
+namespace TechMeetsMagic.Controllers
+{
+    public class AvatarsController : Controller
+    {
+        private readonly TechMeetsMagicContext _context;
+        private readonly IFileServices _fileServices;
+
+        public AvatarsController(TechMeetsMagicContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var ResultingInvetory = _context.
+                .OrderByDescending(y => y.NpcType)
+                .Select(x => new NPCIndexViewModels
+                {
+                    ID = x.ID,
+                    NPCName = x.NPCName,
+                    NPCDescribtion = x.NPCDescribtion,
+                    NpcType = (NPCType)x.NpcType,
+                    NPCLevel = x.NPCLevel,
+                });
+            return View(ResultingInvetory);
+        }
+    }
+}
