0f27a392cb0fae91c4256f1e73f994a72bf046fa
opilane <StenUesson@gmail.com>
2024-11-20 15:20:43 +0200

    added accounts interface, with service

---
+++ TechMeetsMagic.ApplicationsServices/Services/AccountsServices.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechMeetsMagic.Core.Domain;
+using TechMeetsMagic.Core.ServicesInterface;
+namespace TechMeetsMagic.ApplicationsServices.Services
+
+{
+    public class AccountsServices : IAccountServices
+    {
+        private readonly UserManager<ApplicationUser> _UserManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountsServices
+            (
+                UserManager<ApplicationUser> userManager,
+                SignInManager<ApplicationUser> signInManager
+            )
+        {
+            _UserManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
---
+++ TechMeetsMagic.Core/ServicesInterface/IAccountServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechMeetsMagic.Core.Domain;
+using TechMeetsMagic.Core.Dto;
+
+namespace TechMeetsMagic.Core.ServicesInterface
+{
+    public interface IAccountServices
+    {
+    }
+}
