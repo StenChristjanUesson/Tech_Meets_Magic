96dfffb611139e780cf2ad0d49f22e5f0d6e21b4
opilane <StenUesson@gmail.com>
2024-11-27 14:00:50 +0200

    Confrimations for add password and change has been made also controller has been updated

--- TechMeetsMagic/Controllers/AccountsController.cs
+++ TechMeetsMagic/Controllers/AccountsController.cs
@@ -58,6 +58,35 @@ namespace TechMeetsMagic.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user != null)
+                {
+                    return RedirectToAction("Login");
+                }
+                var result = await _userManager.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                    return View();
+                }
+                await _signInManager.RefreshSignInAsync(user);
+                return View("ChangePasswordConfirmation");
+            }
+            return View(model);
+        }
     }
 }
