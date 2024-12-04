4da993478f7988f14578b76a7f4741e56a1d027b
opilane <StenUesson@gmail.com>
2024-12-04 15:13:00 +0200

    Emails Branch, Controller, Service, IService, View and Model have been created

---
+++ TechMeetsMagic/Controllers/EmailsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechMeetsMagic.Core.Dto;
+using TechMeetsMagic.Core.ServicesInterface;
+using TechMeetsMagic.Models.Emails;
+
+namespace TechMeetsMagic.Controllers
+{
+    public class EmailsController : Controller
+    {
+        private readonly IEmailsServices _emailServices;
+        public EmailsController(IEmailsServices emailServices)
+        {
+            _emailServices = emailServices;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailViewModel viewModel)
+        {
+            var dto = new EmailDto()
+            {
+                To = viewModel.To,
+                Subject = viewModel.Subject,
+                Body = viewModel.Body,
+            };
+            _emailServices.SendEmail(dto);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ TechMeetsMagic/Models/Emails/EmailViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TechMeetsMagic.Models.Emails
+{
+    public class EmailViewModel
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
